7bbfa7a86b7c21e23603ce056cb5bc1bb19e4c7e
BURAK AVLAR <2016113029@cumhuriyet.edu.tr>
2024-06-07 14:24:02 +0300

    AppUser ve Vehicle modelleri arasındaki ilişki kuruldu

--- GarajYeriModels/AppUser.cs
+++ GarajYeriModels/AppUser.cs
@@ -11,5 +11,6 @@ namespace GarajYeriModels
         public string FullName { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
+        public virtual ICollection<Vehicle> Vehicles { get; set; } = new List<Vehicle>();
     }
 }
