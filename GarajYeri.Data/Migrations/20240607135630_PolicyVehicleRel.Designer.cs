f509dd2bf7b1719f1628b8472b0cbc84ed1d2e7e
BURAK AVLAR <2016113029@cumhuriyet.edu.tr>
2024-06-07 23:51:56 +0300

    Migration yapıldı

--- GarajYeri.Data/Migrations/20240607135630_PolicyVehicleRel.Designer.cs
+++ GarajYeri.Data/Migrations/20240607135630_PolicyVehicleRel.Designer.cs
@@ -4,6 +4,7 @@ using GarajYeri.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GarajYeri.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240607135630_PolicyVehicleRel")]
+    partial class PolicyVehicleRel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
