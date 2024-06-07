04ee011e0b36292e0895168fa75d3040780002ad
BURAK AVLAR <2016113029@cumhuriyet.edu.tr>
2024-06-07 15:27:17 +0300

    Program.cs ayarları yapıldı

--- GarajYeri.Web/Program.cs
+++ GarajYeri.Web/Program.cs
@@ -1,7 +1,11 @@
+using GarajYeri.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer());
 
 var app = builder.Build();
 
