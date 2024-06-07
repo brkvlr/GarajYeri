4514c138dd1b2317f5d2cc92424fde382078b601
BURAK AVLAR <2016113029@cumhuriyet.edu.tr>
2024-06-07 14:27:44 +0300

    VehiclePhoto ile Vehicle modelleri arasında ilişki kuruldu

---
+++ GarajYeriModels/VehiclePhoto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarajYeriModels
+{
+    public class VehiclePhoto:BaseModel
+    {
+        public string Path { get; set; } // dosya yolu veya adı
+        public int VehicleId { get; set; }
+        public virtual Vehicle Vehicle { get; set; }
+    }
+}
