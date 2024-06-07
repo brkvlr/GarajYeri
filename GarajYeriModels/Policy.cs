7f3ae256ef179e705ff8b190acca290803ad02df
BURAK AVLAR <2016113029@cumhuriyet.edu.tr>
2024-06-07 14:44:38 +0300

    Policy modeli oluşturuldu

---
+++ GarajYeriModels/Policy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GarajYeriModels
+{
+    public class Policy:BaseModel
+    {
+        public string Name { get; set; }
+        public string CompanyName { get; set; } // sigorta firması
+        public DateTime Validity { get; set; }
+
+    }
+}
