397d82383dcc4688f21b4c312306258681e3bbc9
BURAK AVLAR <2016113029@cumhuriyet.edu.tr>
2024-06-11 10:06:00 +0300

    Tema giydirildi

--- GarajYeri.Web/Controllers/HomeController.cs
+++ GarajYeri.Web/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using GarajYeri.Web.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace GarajYeri.Web.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
