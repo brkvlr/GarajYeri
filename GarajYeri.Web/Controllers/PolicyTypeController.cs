6f071a9c7fc98c213b838e2a7cd7b2fd0cb4c54f
BURAK AVLAR <2016113029@cumhuriyet.edu.tr>
2024-06-12 14:57:27 +0300

    PolicyTypesController oluşturuldu

---
+++ GarajYeri.Web/Controllers/PolicyTypeController.cs
@@ -0,0 +1,81 @@
+﻿using GarajYeri.Data;
+using GarajYeriModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GarajYeri.Web.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class PolicyTypeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public PolicyTypeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet] // default geliyor zaten
+        public IActionResult GetAll()
+        {
+            return Json(new { data = _context.PolicyTypes.Where(pt => !pt.IsDeleted) });
+        }
+
+        [HttpPost]
+        public IActionResult Add(PolicyType policyType)
+        {
+            _context.PolicyTypes.Add(policyType);
+            _context.SaveChanges();
+            return Ok(policyType);
+        }
+
+        [HttpPost]
+        public IActionResult HardDelete(PolicyType policyType)
+        {
+            _context.PolicyTypes.Remove(policyType);
+            _context.SaveChanges();
+            return Ok();
+        }
+
+        [HttpPost]
+        public IActionResult SoftDelete(int id)
+        {
+           var policyType = _context.PolicyTypes.Find(id);
+            if (policyType == null)
+            {
+                policyType.IsDeleted = true;
+                _context.PolicyTypes.Update(policyType);
+
+                try
+                {
+                    _context.SaveChanges();
+                    return Ok(policyType);
+                }
+                catch (Exception ex)
+                {
+                   // return StatusCode(500, ex.Message);
+                    return BadRequest(ex);
+                }
+                
+            }
+            else
+            {
+                return BadRequest("Gönderilen ID geçersizdir");
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Update(PolicyType policyType)
+        {
+            _context.PolicyTypes.Update(policyType);
+            _context.SaveChanges();
+            return Ok(policyType);
+        }
+    }
+}
